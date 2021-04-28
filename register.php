6e98e3b6914b215f4206ce2b5c05a83b6f0e0053
Jefferson Yanqui <jeff.0318@hotmail.com>
2021-04-27 19:23:05 -0500

    first commit

---
+++ register.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <a href="index.php">Inicio</a>
+    <h1>Registro de Usuario</h1>
+        <form method="POST">
+            <label>Nombres</label>
+            <input><br>
+            <label>Apellidos</label>
+            <input><br>
+            <label>Cedula</label>
+            <input><br>
+            <label>Fecha de Nacimiento</label>
+            <input><br>
+            <label>Genero</label>
+            <input><br>
+            <label>Provincia</label>
+            <input><br>
+            <label>Ciudad</label>
+            <input><br>
+            <label>Direccion</label>
+            <input><br>
+            <label>Codigo Postal</label>
+            <input><br>
+            <label>Telefono Fijo</label>
+            <input><br>
+            <label>Telefono Movil</label>
+            <input><br>
+            <label>Elije un Letra</label>
+            <select>
+            <option>A</option>
+            <option>B</option>
+            <option>C</option>
+            </select>
+            <br>
+            <label>Correo Electronico</label>
+            <input><br>
+            <label>Contraseña</label>
+            <input><br>
+
+            <button>Registrar</button>
+        </form>
+</body>
+</html>
