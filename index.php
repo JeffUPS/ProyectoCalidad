16c56c84df4a9da7b81a260bd98a41bdea917c30
Jefferson Yanqui <jeff.0318@hotmail.com>
2021-04-27 19:55:38 -0500

    first commit

--- index.php
+++ index.php
@@ -4,20 +4,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel=stylesheet href="css/style.css" type=text/css media=screen>
     <title>Login</title>
 </head>
 <body>
     <h1>Inicio de Sesion</h1>
-    <form method="POST">
-        <label>Correo Electronico</label>
-        <input required>
-        <br>
-        <label>Contraseña</label>
-        <input required>
-        <br>
-        <button>Iniciar Sesion</button>
-        <br>
-        <a href="register.php">Registrar</a>
-    </form>
+        <form method="POST">
+            <label>Correo Electronico</label><br><br>
+            <input type="email" required>
+            <br><br>
+            <label>Contraseña</label><br><br>
+            <input type="password" required>
+            <br><br>
+            <button>Iniciar Sesion</button>
+            <br><br>
+            <a href="register.php">Registrar</a>
+        </form>
 </body>
 </html>
--- register.php
+++ register.php
@@ -4,45 +4,46 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel=stylesheet href="css/style.css" type=text/css media=screen>
     <title>Document</title>
 </head>
 <body>
     <a href="index.php">Inicio</a>
     <h1>Registro de Usuario</h1>
         <form method="POST">
-            <label>Nombres</label>
+            <label>Nombres</label><br>
+            <input type="text"><br>
+            <label>Apellidos</label><br>
+            <input type="text"><br>
+            <label>Cedula</label><br>
+            <input type="text"><br>
+            <label>Fecha de Nacimiento</label><br>
+            <input type="date"><br>
+            <label>Genero</label><br>
             <input><br>
-            <label>Apellidos</label>
-            <input><br>
-            <label>Cedula</label>
-            <input><br>
-            <label>Fecha de Nacimiento</label>
-            <input><br>
-            <label>Genero</label>
-            <input><br>
-            <label>Provincia</label>
-            <input><br>
-            <label>Ciudad</label>
-            <input><br>
-            <label>Direccion</label>
-            <input><br>
-            <label>Codigo Postal</label>
-            <input><br>
-            <label>Telefono Fijo</label>
-            <input><br>
-            <label>Telefono Movil</label>
-            <input><br>
-            <label>Elije un Letra</label>
+            <label>Provincia</label><br>
+            <input type="text"><br>
+            <label>Ciudad</label><br>
+            <input type="text"><br>
+            <label>Direccion</label><br>
+            <input type="text"><br>
+            <label>Codigo Postal</label><br>
+            <input type="text"><br>
+            <label>Telefono Fijo</label><br>
+            <input type="text"><br>
+            <label>Telefono Movil</label><br>
+            <input type="text"><br>
+            <label>Elije un Letra</label><br>
             <select>
             <option>A</option>
             <option>B</option>
             <option>C</option>
             </select>
             <br>
-            <label>Correo Electronico</label>
-            <input><br>
-            <label>Contraseña</label>
-            <input><br>
+            <label>Correo Electronico</label><br>
+            <input type="email"><br>
+            <label type="password">Contraseña</label><br>
+            <input><br><br>
 
             <button>Registrar</button>
         </form>
